3609e8901cdf6f5bcc8ef2e82e92a284f259dba8
Ricky Powell <rickypowell@users.noreply.github.com>
2025-01-02 16:13:43 -0500

    chore: deprecate the older print_countdown

--- crates/example/src/main.rs
+++ crates/example/src/main.rs
@@ -1,18 +1,24 @@
-use countdown;
+use countdown::countdown::callback_countdown;
 use std::io::{self, Write};
 
 fn main() {
     // countdown timer
     println!("Countdown timer: ");
-    countdown::countdown::callback_countdown(20, |seconds| {
-        let escape = "\x1b";
-        let erase_current_line = "[2K";
+    callback_countdown(12, |seconds| {
+        let erase_current_line = "\x1b[2K";
+        let warning = match seconds {
+            // yellow
+            4..=10 => "\x1b[33m",
+            // red
+            0..=3 => "\x1b[31m",
+            // blue
+            _ => "\x1b[34m",
+        };
         print!(
-            "\r{}{}{} seconds remaining",
-            escape, erase_current_line, seconds
+            "\r{}{}{} seconds remaining\x1b[0m",
+            erase_current_line, warning, seconds
         );
         io::stdout().flush().unwrap();
     });
-    println!("\nTime's up!");
-    println!("Hello, world!");
+    println!("\n\x1b[1mTime's up!\x1b[0m");
 }
